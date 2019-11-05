1a3b08c649bf800b502736761e7392044cda3f44
Makavura <makavuramughanga@gmail.com>
2019-11-05 23:49:36 +0300

    scaffold approach

---
+++ mod_one_assignment/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace mod_one_assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Student Information
+
+            string firstNameS = "";
+            string lastNameS ="";
+            string bdS = "";
+            string addressLine1S = "";
+            string addressLine2S = "";
+            string cityS = "";
+            string stateProvinceS = "";
+            string zipPostalS = "";
+            string countryS = "";
+
+                        //  Teacher Information
+
+            string firstNameT = "";
+            string lastNameT ="";
+            string bdT = "";
+            string addressLine1T = "";
+            string addressLine2T = "";
+            string cityT = "";
+            string stateProvinceT = "";
+            string zipPostalT = "";
+            string countryT = "";
+
+            //  UProgram Information
+            string programName = "";
+            string departmentHead = "";
+            string degrees = "";
+
+            // Degree Informartion
+            string degreeName = "";
+            string creditsRequired = "";
+
+            //  Course Informarion
+            string courseName = "";
+            int credits = 0;
+            int durationInWeeks = 0;
+            string teacher = "";
+
+//             birthDate = ("1932, 6, 1");
+// DateTime bD;
+
+//             if ( DateTime.TryParseExact(birthDate, "yyyy-MM-dd", System.Globalization.CultureInfo.InvariantCulture,
+// System.Globalization.DateTimeStyles.None, out bD)){
+//    
+// }
+
+        }
+    }
+}
