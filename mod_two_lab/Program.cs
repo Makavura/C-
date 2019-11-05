0cb5f32f2ff98708770185a630d22c99f18e3d68
Makavura <makavuramughanga@gmail.com>
2019-11-06 00:34:59 +0300

    if statement

---
+++ mod_two_lab/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace mod_two_lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //  an if decision block to check for an even number
+
+            //  Request user input with ReadLine()
+            Console.WriteLine("Please enter an integer value and press Enter.");
+
+            // Assign the entered value to a variable and convert input to integer
+            int input = Int32.Parse(Console.ReadLine());
+
+            // Check to see if number is even by using % | modulus
+
+            if (input % 2 == 0)
+            {
+                Console.WriteLine("The entered number was an even number.");
+            }
+            else
+            {
+                Console.WriteLine("The entered number was not an even number.");
+            }
+        }
+    }
+}
