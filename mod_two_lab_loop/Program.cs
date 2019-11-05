1a4cb515ee393aae33184ca63fa368d369c581f6
Makavura <makavuramughanga@gmail.com>
2019-11-06 00:45:02 +0300

    for loop and nested loop

---
+++ mod_two_lab_loop/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace mod_two_lab_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // for loop that displays values of counter
+
+            for(int counter = 0; counter < 10; counter++)
+            {
+                Console.WriteLine($"Counter is at: {counter}");
+            }
+
+            //  nested for loop
+            //  find prime numbers less than 100
+            int outer;
+            int inner;
+
+            for ( outer = 2; outer < 100; outer++)
+            {
+                for (inner = 2; inner <= (outer/inner); inner++ )
+                
+                    if((outer % inner) == 0) break;
+                if (inner > (outer/inner))
+                    Console.WriteLine("{0} is prime", outer);
+                
+            }
+        }
+    }
+}
