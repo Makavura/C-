5567bbbc1c9cc5815d8a63fea427f17223ebf1d3
Makavura <makavuramughanga@gmail.com>
2019-11-06 08:08:54 +0300

    while & do loop

---
+++ mod_two_while_do_lab/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace mod_two_while_do_lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // WHILE LOOP
+            int n = 1;
+            while (n < 6)
+            {
+                Console.WriteLine($"Current value of n is {n}");
+                n++;
+            }
+
+            // DO LOOP
+            int x = 0;
+            do{
+                Console.WriteLine(x);
+                x++;
+            } while ( x<5);
+        }
+    }
+}
