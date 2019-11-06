50adf0156f2bb6d04ce072cab3f3b0d7e2faff9d
Makavura <makavuramughanga@gmail.com>
2019-11-06 14:11:47 +0300

    encapsulation notes

--- OOP in C#/OOP/Program.cs
+++ OOP in C#/OOP/Program.cs
@@ -66,6 +66,8 @@ namespace OOP
             // define event
             // delegate definition not shown
             public event OutOfBeansHandler OutOfBeans;
+            // Program.cs(68,26): error CS0246: The type or namespace name 'OutOfBeansHandler' could not be found (are you missing a using directive or an assembly reference?) [/home/makavura/.senior/Learning/C#/OOP in C#/OOP/OOP.csproj]
+
 
 
 
@@ -97,9 +99,54 @@ namespace OOP
                 // Method logic goes here.
             }
         }
+
+        // INSTANTIATING A CLASS
+        // a class is a blueprint for a type
+        // to use its behaviours and characteristics, you need to instantiate the class
+        //  an instance of a class is an object
+
+        DrinksMachine dm = new DrinksMachine();
+        // create an object in memory based on DrinksMachine type
+        // create an object reference named dm that refers to new DrinksMachine object
+        // type inference
+        // -allow compiler to deduce object type at compile time
+        // to use type inference use var keyword, like so
+
+// ENCAPSULATION
+// -used to descirbe accessibility of members inside a class
+// -also defined as act of including all data and behaviour required of a class
+// within a class definition
+
+// Access modifier	Description
+// public	The type is available to code running in any assembly that references the assembly in which the class is contained.
+// internal	The type is available to any code within the same assembly, but not available to code in another assembly.
+// private	The type is only available to code within the class that contains it. You can only use the private access modifier with nested classes. This is the default value if you do not specify an access modifier.
+// protected	The type is only accessible within its class and by derived class instances.
+
+// properties can be a part of encapsulation
+// permit users oof a class a means of getting and setting values
+// for the private member fields within class
+// setting members private is known as a form of hiding
+
+// hide implementation or verification code
+// Properties also present an "interface" to your class by exposing a way to get or 
+// set the members of the class that the user can trust. In other words, if you have
+//  a property called public Birthdate(date birth), that accepts a birthdate from a user,
+//  you can implement the validation code in anyway you see fit, such as using 
+// regular expressions to validate or perhaps some custom logic to verify the date
+//  range, and then later change that validation logic without impacting the use of 
+// the property. Users still just pass in a birthdate in date format.
         static void Main(string[] args)
         {
-
+            var dm = new DrinksMachine();
+            // causes no change in application runtime, simply a shortcut to avoid
+            // typing the class name twice
+            // after instantiating object, you can use any of the members that 
+            // are defined in class
+            // dot notation
+            dm.Make = "Fourth Coffee";
+            dm.Model = "BeanCrusher 3000";
+            dm.MakeEspresso();
         }
     }
 }
