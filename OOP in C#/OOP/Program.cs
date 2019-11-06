8979d825ae9cc7cf13030287060a4a9d222e7298
Makavura <makavuramughanga@gmail.com>
2019-11-06 16:14:21 +0300

    ...

--- OOP in C#/OOP/Program.cs
+++ OOP in C#/OOP/Program.cs
@@ -51,7 +51,29 @@ namespace OOP
                 }
             }
             // define properties
-            public string Make { get; set; }
+            public int Age
+            {
+                get
+                {
+                    return Age;
+                }
+                set
+                {
+                    Age = value;
+                }
+            }
+            public string Make
+            {
+                get
+                {
+                    return make;
+                }
+                set
+                {
+                    Make = value;
+                }
+            }
+            // auto-implemented property
             public string Model { get; set; }
             // define methods
             public void MakeCappuccino()
@@ -68,11 +90,27 @@ namespace OOP
             public event OutOfBeansHandler OutOfBeans;
             // Program.cs(68,26): error CS0246: The type or namespace name 'OutOfBeansHandler' could not be found (are you missing a using directive or an assembly reference?) [/home/makavura/.senior/Learning/C#/OOP in C#/OOP/OOP.csproj]
 
-
-
-
+        }
+        // Constructors
+        public DrinksMachine(int Age)
+        {
+            this.Age = Age;
         }
 
+
+        public DrinksMachine(string make, string model)
+        {
+            this.Make = make;
+            this.Model = model;
+        }
+
+
+        public DrinksMachine(int age, string make, string model)
+        {
+            this.Age = age;
+            this.Make = make;
+            this.Model = model;
+        }
         // PARIAL CLASSES
         // split definition of the class across multiple source files
         // combined during compilation
@@ -112,30 +150,119 @@ namespace OOP
         // -allow compiler to deduce object type at compile time
         // to use type inference use var keyword, like so
 
-// ENCAPSULATION
-// -used to descirbe accessibility of members inside a class
-// -also defined as act of including all data and behaviour required of a class
-// within a class definition
+        // ENCAPSULATION
+        // -used to descirbe accessibility of members inside a class
+        // -also defined as act of including all data and behaviour required of a class
+        // within a class definition
 
-// Access modifier	Description
-// public	The type is available to code running in any assembly that references the assembly in which the class is contained.
-// internal	The type is available to any code within the same assembly, but not available to code in another assembly.
-// private	The type is only available to code within the class that contains it. You can only use the private access modifier with nested classes. This is the default value if you do not specify an access modifier.
-// protected	The type is only accessible within its class and by derived class instances.
+        // Access modifier	Description
+        // public	The type is available to code running in any assembly that references the assembly in which the class is contained.
+        // internal	The type is available to any code within the same assembly, but not available to code in another assembly.
+        // private	The type is only available to code within the class that contains it. You can only use the private access modifier with nested classes. This is the default value if you do not specify an access modifier.
+        // protected	The type is only accessible within its class and by derived class instances.
 
-// properties can be a part of encapsulation
-// permit users oof a class a means of getting and setting values
-// for the private member fields within class
-// setting members private is known as a form of hiding
+        // properties can be a part of encapsulation
+        // permit users oof a class a means of getting and setting values
+        // for the private member fields within class
+        // setting members private is known as a form of hiding
 
-// hide implementation or verification code
-// Properties also present an "interface" to your class by exposing a way to get or 
-// set the members of the class that the user can trust. In other words, if you have
-//  a property called public Birthdate(date birth), that accepts a birthdate from a user,
-//  you can implement the validation code in anyway you see fit, such as using 
-// regular expressions to validate or perhaps some custom logic to verify the date
-//  range, and then later change that validation logic without impacting the use of 
-// the property. Users still just pass in a birthdate in date format.
+        // hide implementation or verification code
+        // Properties also present an "interface" to your class by exposing a way to get or 
+        // set the members of the class that the user can trust. In other words, if you have
+        //  a property called public Birthdate(date birth), that accepts a birthdate from a user,
+        //  you can implement the validation code in anyway you see fit, such as using 
+        // regular expressions to validate or perhaps some custom logic to verify the date
+        //  range, and then later change that validation logic without impacting the use of 
+        // the property. Users still just pass in a birthdate in date format.
+
+                // two basic property types of properties in a class
+        // read-only or read-write
+        // a get property accessor is used to return the property value
+        // a set accessor is used to assign a new value, ommission makes it read only
+        // a value keyword is used to define value being assigned to set accessor
+        // for simple properties that require no custom accessor code, consider the
+        // option of using auto-implemented properties
+
+        // auto-implemented properties
+        public double TotalPurchases { get; set; }
+        public string Name { get; set;}
+        public int CustomerID { get; set; }
+
+        // CONSTRUCTORS
+        public class DrinksMachine
+        {
+            public int Age {
+                get;
+                set;
+            }
+            public DrinksMachine()
+            {
+                Age = 0;
+            }
+        }
+
+// adding Multiple constructors
+public class DrinksMachine
+{
+    public int Age {get; set;}
+    public string Make {get; set;}
+    public string Model {get; set;}
+    public DrinksMachine(int age)
+    {
+        this.Age = age;
+    }
+    public DrinksMachine(string make, string model)
+    {
+        this.Make = make;
+        this.Model = model;
+    }
+    public DrinksMachine(int age, string make, string model)
+    {
+        this.Age = age;
+        this.Make = make;
+        this.Model = model;
+    }
+}
+
+// Calling Constructors
+var dm1 = new DrinksMachine(2);
+var dm2 = new DrinksMachine("Fourth Coffee", "BeanCrusher 3000");
+var dm3 = new DrinksMachine(3,"Fourth Coffee", "BeanToaster turbo");
+
+    class Program
+    {
+
+// in the case of a need to create a class purely to encapsulate useful functionality rather 
+// than to represemt an instance of anything
+// eg methods that convert imperial weights and measure to metric weights and measures and vice versa
+
+// in scenarios like this, a static class has the best use case
+// a static class is a class that cannot be instantiated
+// use static keyword
+// any members within must also use static keyword
+
+
+        public static class Conversion
+        {
+            public static double PoundsToKilos(double pounds)
+            {
+                double kilos = pounds * 0.4536;
+                return kilos;
+            }
+            public static double KilosToPounds(double kilos)
+            {
+                double pounds = kilos * 2.205;
+                return pounds;
+            }
+        }
+        static void Main(string[] args)
+        {
+            double weightInKilos = 80;
+            double weightInPounds = Conversion.KilosToPounds(weightInKilos);
+            Console.WriteLine(weightInPounds);
+
+        }
+    }
         static void Main(string[] args)
         {
             var dm = new DrinksMachine();
