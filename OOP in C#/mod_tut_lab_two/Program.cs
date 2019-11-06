6e23c14cc549b59eea91ea317b69d976b5f94dea
Makavura <makavuramughanga@gmail.com>
2019-11-06 20:08:06 +0300

    code lab

---
+++ OOP in C#/mod_tut_lab_two/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace mod_tut_lab_two
+{
+    // CONSTRUCTORS AND STATIC MEMBERS
+// This lab will build on the concepts covered in the previous lab (Module 1 Tutorial Lab 1). 
+// Using the preexisting and instantiated Car() class, you will practice adding constructors to a 
+// class and implementing static class variables.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
