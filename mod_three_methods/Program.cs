bbb925a5c46701731687ff9b4587274e755e22e4
Makavura <makavuramughanga@gmail.com>
2019-11-06 11:10:14 +0300

    method call

--- mod_three_methods/Program.cs
+++ mod_three_methods/Program.cs
@@ -6,7 +6,19 @@ namespace mod_three_methods
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+Sum(20, 40);
         }
+
+        
+// Sum() method that takes two integer arguments and sums them
+// The method returns no value which is why we use void
+// We also need to use static in the method signature because Main is static
+// and you cannot call a non-static method from a static method
+
+            static void Sum(int first, int second)
+            {
+                int sum = first + second;
+                Console.WriteLine($"Th sum of {first} and {second} is {sum}");
+            }
     }
 }
