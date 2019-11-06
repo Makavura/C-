c1dd425b00fc7180146e66547b9318db24367542
Makavura <makavuramughanga@gmail.com>
2019-11-06 11:25:25 +0300

    exception handling

---
+++ mod_three_exception_handling/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace mod_three_exception_handling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+// practice exception handling techniques
+// exceptions will happen in code
+// planning for and coding properly
+// make your application behave in a professional manner
+
+int result = Divide(2, 0);
+
+        }
+
+        // method divide() has no validation and with no input invalidation or error catching
+// the application will crash
+
+static int Divide(int first, int second)
+{
+    int result = first /second;
+    return result;
+}
+
+    }
+}
