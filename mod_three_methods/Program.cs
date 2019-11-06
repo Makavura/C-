cf72c96372eeff3c9db47f4e0ef061e7129952d3
Makavura <makavuramughanga@gmail.com>
2019-11-06 11:07:11 +0300

    creating and using methods lab

---
+++ mod_three_methods/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mod_three_methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
