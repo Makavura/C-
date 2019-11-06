cf40cd6f5233de7bdb3d84e60a403bac78680cd6
Makavura <makavuramughanga@gmail.com>
2019-11-06 11:28:48 +0300

    try catch

--- mod_three_exception_handling/Program.cs
+++ mod_three_exception_handling/Program.cs
@@ -20,7 +20,16 @@ int result = Divide(2, 0);
 
 static int Divide(int first, int second)
 {
-    int result = first /second;
+    int result = 0;
+    
+    try
+    {
+result = first /second;
+    }
+    catch (DivideByZeroException ex)
+    {
+        Console.WriteLine("Cannot divide by zero, please provide a non-zero  value for your second value");
+    }
     return result;
 }
 
