482e543addb936f7ce7f0156e322d6de83558dc1
Makavura <makavuramughanga@gmail.com>
2019-11-06 09:11:34 +0300

    chess challenge

---
+++ chessChallenge/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace chessChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string _c;
+            
+            for(int counter = 0; counter < 8; counter++)
+            {
+                if( (counter % 2) == 0)
+                {
+                    _c = "X";
+                }
+                else{
+                    _c = "O";
+                }
+
+                Console.Write(_c);
+            }
+
+
+        }
+    }
+}
