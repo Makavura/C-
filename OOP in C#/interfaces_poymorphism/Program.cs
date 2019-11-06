19e316e532738381268177e49e74557773b1f59f
Makavura <makavuramughanga@gmail.com>
2019-11-06 21:37:08 +0300

    notes | Zzz

--- OOP in C#/interfaces_poymorphism/Program.cs
+++ OOP in C#/interfaces_poymorphism/Program.cs
@@ -4,9 +4,22 @@ namespace interfaces_poymorphism
 {
     class Program
     {
+
+        // INTERFACES
+
+        // an interface is a class without an implementation
+        // specifies a set of charecteristics and behaviours by defining signatures methods, properties
+        // evemts amd indexers without specifying how any of these members are implemented
+        
+
+        // classes implememt interfaces
+
+        // by implementing the interface the class is guaranteeing that it will provide
+        // the funcctionality provided by the interface
+
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
         }
     }
 }
