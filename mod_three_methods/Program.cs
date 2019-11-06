2650e8a8bd3db0b935c7ca6d40c8e895ab1963ba
Makavura <makavuramughanga@gmail.com>
2019-11-06 11:19:10 +0300

    method overloading

--- mod_three_methods/Program.cs
+++ mod_three_methods/Program.cs
@@ -7,7 +7,13 @@ namespace mod_three_methods
         static void Main(string[] args)
         {
             int result = Sum(20, 40);
-            Console.WriteLine($"Th sum of 20 and 40 is {result}");
+            Console.WriteLine($"The sum of 20 and 40 is {result}");
+
+            int result3 = Sum(10, 50, 80);
+            Console.WriteLine($"The sum of 10, 50 and 80 is {result3}");
+
+            double dblResult = Sum(21.7, 30.87);
+            Console.WriteLine($"The sum of 21.7 and 30.87 is {dblResult}");
         }
 
 
@@ -22,5 +28,20 @@ namespace mod_three_methods
             return sum;
 
         }
+        // overloading
+        // compiler will know which method to call based on argument data types
+        static int Sum(int first, int second, int third)
+        {
+            int sum = first + second + third;
+            return sum;
+        }
+        // overloading again
+        static double Sum(double first, double second)
+        {
+            double result = first + second;
+            return result;
+        }
+
+
     }
 }
