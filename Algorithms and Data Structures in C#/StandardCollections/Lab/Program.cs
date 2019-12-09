1fe8e006e64455ddc4adffc1d91ff36efbcd21f1
Makavura <makavuramughanga@gmail.com>
2019-12-09 16:14:52 +0300

    second iteration

--- Algorithms and Data Structures in C#/StandardCollections/Lab/Program.cs
+++ Algorithms and Data Structures in C#/StandardCollections/Lab/Program.cs
@@ -7,9 +7,17 @@ namespace Lab {
 
             ArrayList myArrList = new ArrayList ();
             myArrList.Add ("First Item");
-            myArrList.Add (3);
-            myArrList.Add (4.5);
+            myArrList.Add ("Third Item");
+            myArrList.Add ("Second Item");
 
+            myArrList.Sort ();
+            int itemIndex = myArrList.IndexOf ("Third Item");
+
+            foreach (object obj in myArrList) {
+                Console.WriteLine (obj.ToString ());
+            }
+            Console.WriteLine ();
+            Console.WriteLine ($"Third Item is at index {itemIndex}.");
             foreach (object obj in myArrList) {
                 Console.WriteLine (obj.ToString ());
             }
