ef5b6e460761516fcc34081305c764a25e9520a6
Makavura <makavuramughanga@gmail.com>
2019-12-09 12:05:50 +0300

    vroom

--- Algorithms and Data Structures in C#/Data Structures/Structs/Program.cs
+++ Algorithms and Data Structures in C#/Data Structures/Structs/Program.cs
@@ -2,12 +2,32 @@
 
 namespace Structs
 {
+
+
+    public struct  car
+    {
+        public string Make;
+        public string Model;
+        public string Color;
+
+        pub;ic void Accelerate(){
+            Console.WriteLine("Vrooooooom!");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
 
-            
+car myCar = new car();
+
+myCar.Make = "Chevy";
+myCar.Model = "Silverado";
+myCar.Color = "Gray";
+
+Console.WriteLine($"My car is a {myCar.Make} {myCar.Model} that is {myCar.Color} in color")
+            Console.WriteLine();
+            myCar.Accelerate();
         }
     }
 }
