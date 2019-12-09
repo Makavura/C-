e196282ff75b95cf9fe0576cb0872088abef8bee
Makavura <makavuramughanga@gmail.com>
2019-12-09 13:01:37 +0300

    error prone

--- Algorithms and Data Structures in C#/Data Structures/Lab/Program.cs
+++ Algorithms and Data Structures in C#/Data Structures/Lab/Program.cs
@@ -1,24 +1,42 @@
 ﻿using System;
 
-namespace Lab
-{
+namespace Lab {
 
-public struct Student
-{
+    public struct Student {
 
-}
+        public int grade;
+        public int AdmNo;
 
-public struct Teacher
-{
+        public void Study () {
+            Console.WriteLine ("Read");
+        }
 
-}
+    }
 
+    public struct Teacher {
 
-    class Program
-    {
+        public int isHOD;
+        public int TSCNO;
 
-public void manualArrayWork(){
-            int[] numbers  = new int[5];
+        public void gradeAssignments () {
+            Console.WriteLine ("Do not be a sadist, mwalimu");
+        }
+    }
+    class Program {
+
+        static void Main (string[] args) {
+
+            int _students[] = {
+
+                _student = new
+            }
+
+        }
+    }
+
+    class arrayS {
+        public void manualArrayWork () {
+            int[] numbers = new int[5];
 
             numbers[0] = 2;
             numbers[1] = 5;
@@ -26,29 +44,20 @@ public void manualArrayWork(){
             numbers[3] = 6;
             numbers[4] = 7;
 
-            Console.WriteLine(numbers[0]);
-                    Console.WriteLine(numbers[3]);
-                        Console.WriteLine(numbers[2]);
-            
+            Console.WriteLine (numbers[0]);
+            Console.WriteLine (numbers[3]);
+            Console.WriteLine (numbers[2]);
+
             int total = 0;
-            for(int i = 0; i < numbers.Length; i++)
-            {
+            for (int i = 0; i < numbers.Length; i++) {
                 total = total + numbers[i];
             }
-            Console.WriteLine(total.ToString());
+            Console.WriteLine (total.ToString ());
 
             int forEachTotal = 0;
-            foreach(int num in numbers)
-            {
+            foreach (int num in numbers) {
                 forEachTotal = forEachTotal + num;
             }
-            Console.WriteLine(forEachTotal);
-} 
-
-        static void Main(string[] args)
-        {
-
-
+            Console.WriteLine (forEachTotal);
         }
     }
-}
