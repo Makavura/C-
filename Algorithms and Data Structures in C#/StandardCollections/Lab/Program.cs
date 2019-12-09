ed5060731e79fb678714393d7fcb949055088666
Makavura <makavuramughanga@gmail.com>
2019-12-09 16:14:05 +0300

    first iteration

--- Algorithms and Data Structures in C#/StandardCollections/Lab/Program.cs
+++ Algorithms and Data Structures in C#/StandardCollections/Lab/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Collections;
 
-namespace Lab
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace Lab {
+    class Program {
+        static void Main (string[] args) {
+
+            ArrayList myArrList = new ArrayList ();
+            myArrList.Add ("First Item");
+            myArrList.Add (3);
+            myArrList.Add (4.5);
+
+            foreach (object obj in myArrList) {
+                Console.WriteLine (obj.ToString ());
+            }
+
         }
     }
 }
