4b71470a20ccf9e91ee357fa58ef2bb0b0a1719b
Makavura <makavuramughanga@gmail.com>
2019-12-09 12:16:20 +0300

    ...

--- Algorithms and Data Structures in C#/Data Structures/Structs/Program.cs
+++ Algorithms and Data Structures in C#/Data Structures/Structs/Program.cs
@@ -14,11 +14,25 @@ namespace Structs
             Console.WriteLine("Vrooooooom!");
         }
     }
+
+    enum Days { Sun, Mon, Tue, Wed, Thu, Fri, Sat };
+
     class Program
     {
         static void Main(string[] args)
         {
+// ENUM
 
+int x = (int)Days.Sun;
+int y = (int)Days.Fri;
+
+Console.WriteLine("Sun = {0}", x);
+Console.WriteLine("Fri = {0}", y);
+Console.WriteLine(Days.Thu);
+
+
+
+// STRUCTS
 car myCar = new car();
 
 myCar.Make = "Chevy";
@@ -28,6 +42,9 @@ myCar.Color = "Gray";
 Console.WriteLine($"My car is a {myCar.Make} {myCar.Model} that is {myCar.Color} in color");
             Console.WriteLine();
             myCar.Accelerate();
+
+
+
         }
     }
 }
