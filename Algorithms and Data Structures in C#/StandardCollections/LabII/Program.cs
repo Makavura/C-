09d2c95b6671369f67145222d441f59d60523f57
Makavura <makavuramughanga@gmail.com>
2019-12-09 16:24:42 +0300

    LabII

---
+++ Algorithms and Data Structures in C#/StandardCollections/LabII/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LabII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
