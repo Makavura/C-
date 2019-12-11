21eb57dbfbe7c87dde7baee86e3bd06f08690946
Makavura <makavuramughanga@gmail.com>
2019-12-11 12:42:21 +0300

    ...

--- OOP in C#/SALABS/SALAB01/Program.cs
+++ OOP in C#/SALABS/SALAB01/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SALAB01 {
 
@@ -7,8 +8,7 @@ namespace SALAB01 {
         public string Name { get; set; }
         public string admNo { get; set; }
 
-        public Student(string name, string admno)
-        {
+        public Student (string name, string admno) {
             Name = name;
             admNo = admno;
         }
@@ -18,28 +18,76 @@ namespace SALAB01 {
         public string Name { get; set; }
         public string TSCNo { get; set; }
 
-        public Student(string name, string tscno)
-        {
+        public Teacher (string name, string tscno) {
             Name = name;
             TSCNo = tscno;
         }
     }
 
     public class UProgram {
+        public string UName { get; set; }
+        public string Uabbrv { get; set; }
 
+        public UProgram (string uname, string uabbrv) {
+            UName = uname;
+            Uabbrv = uabbrv;
+        }
 
-    }
+        public class Degree {
+            public string DName { get; set; }
+            public string DCode { get; set; }
 
-    public class Degree {
+            public Degree (string dname, string dcode) {
+                DName = dname;
+                DCode = dcode;
+            }
 
-    }
+            public class Course {
+                public string CName { get; set; }
+                public string CCode { get; set; }
 
-    public class Course {
+                public List<Teacher> _courseTeachers = new List<Teacher> ();
+
+                public List<Teacher> addTeacherToCourse (string tName, string tNo) {
+
+                    Teacher teacherToAddToCourse = new Teacher (tName, tNo);
+                    _courseTeachers.Add (teacherToAddToCourse);
+
+                    foreach (Teacher aMwalimu in _courseTeachers) {
+                        Console.WriteLine (aMwalimu);
+                    }
+
+                    return _courseTeachers;
+                }
+
+                public Course (string cname, string ccode) {
+                    CName = cname;
+                    CCode = ccode;
+                }
+            }
+
+        }
 
     }
 
     class Program {
         static void Main (string[] args) {
+            Student _studenti = new Student ("Marcus Hopsin", "FV007");
+            Console.WriteLine ("Student Name = {0} Number = {1}", _studenti.Name, _studenti.admNo);
+
+            Student _studentii = new Student ("Kendrick Lamar", "TDE");
+            Console.WriteLine ("Student Name = {0} Number = {1}", _studentii.Name, _studentii.admNo);
+
+            Student _studentiii = new Student ("Logic", "DefJam");
+            Console.WriteLine ("Student Name = {0} Number = {1}", _studentiii.Name, _studentiii.admNo);
+
+            Teacher _teacheri = new Teacher ("Shady", "SR");
+            Console.WriteLine ("Teacher = {0} TSCNUMBER = {1}", _teacheri.Name, _teacheri.TSCNo);
+
+            var _CourseIinstance = new UProgram.Degree.Course ("Rap", "Flow101");
+            var _CourseIIinstance = new UProgram.Degree.Course ("Programming with C#", "C#101");
+
+            Console.WriteLine(ToString(_CourseIinstance), ToString(_CourseIIinstance));
 
         }
     }
