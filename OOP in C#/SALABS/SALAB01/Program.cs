cd922e053e00032f9c2e3127c2d7692527053de8
Makavura <makavuramughanga@gmail.com>
2019-12-11 11:58:11 +0300

    SALAB01

---
+++ OOP in C#/SALABS/SALAB01/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace SALAB01 {
+
+    public class Student {
+
+        public string Name { get; set; }
+        public string admNo { get; set; }
+
+        public Student(string name, string admno)
+        {
+            Name = name;
+            admNo = admno;
+        }
+    }
+
+    public class Teacher {
+        public string Name { get; set; }
+        public string TSCNo { get; set; }
+
+        public Student(string name, string tscno)
+        {
+            Name = name;
+            TSCNo = tscno;
+        }
+    }
+
+    public class UProgram {
+
+
+    }
+
+    public class Degree {
+
+    }
+
+    public class Course {
+
+    }
+
+    class Program {
+        static void Main (string[] args) {
+
+        }
+    }
+}
